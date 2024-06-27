01c0665ab10898f6894c5ada8fffa6328e11cc07
deepakpant <deepak.93p@gmail.com>
2024-06-27 14:52:51 +0530

    added more scripts

--- helper/check_pck_mgr.sh
+++ helper/check_pck_mgr.sh
@@ -3,15 +3,15 @@
 # Check for existence of apt executable
 if which apt >/dev/null 2>&1; then
   echo "apt"
-  exit 0
+  exit 1
 fi
 
 # Check for existence of yum executable
 if which yum >/dev/null 2>&1; then
   echo "yum"
-  exit 0
+  exit 2
 fi
 
 # Package manager not definitively identified (could be something else)
 echo "other"
-exit 1
+exit 0
